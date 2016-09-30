1bda717c3f8a51fd221707302dc0288bce0740a1
Richard Kopelow <richardkopelow@gmail.com>
2016-09-30 14:35:35 -0400

    Wrapped "Richard Kopelow" header and the games iconcontainer in a surface

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,39 @@ define(function (require, exports, module) {
     var Context = require('samsara/dom/Context');
     var Surface = require('samsara/dom/Surface');
     var ContainerSurface = require('samsara/dom/ContainerSurface');
+    var Transitionable = require('samsara/core/Transitionable');
     var SideBar = require('app/SideBar');
     var IconContainer = require('app/IconContainer');
 
+    var contentSize = new Transitionable([0, 0]);
+    var content = new ContainerSurface({
+        size: contentSize,
+        origin: [1, 0.5]
+    });
+
+    var nameSurface = new Surface({
+        size: [true,true],
+        origin: [0, 0],
+        content: 'Richard Kopelow',
+        properties: {
+            fontSize: '40px'
+        }
+    });
+    content
+        .add({align: [0, 0]})
+        .add(nameSurface);
+
+    var sidebarWidth = 90;
     var sideBar = SideBar({
-        size: [90, undefined],
+        size: [sidebarWidth, undefined],
         origin: [0, 0.5],
         opacity: 1,
-        iconSize: 80,
+        iconSize: sidebarWidth - 10,
         transition: {duration: 500,curve: 'easeOut'},
         content: [{src: 'Assets/unityLogos/unity-logo.png'},
                   {src: 'http://3.bp.blogspot.com/-PTty3CfTGnA/TpZOEjTQ_WI/AAAAAAAAAeo/KeKt_D5X2xo/s1600/js.jpg'}],
         properties: {
             background: 'Black',
-            //borderRadius: '20px'
         }
     });
 
@@ -36,11 +55,19 @@ define(function (require, exports, module) {
     var context = new Context();
 
     context
+        .add({align: [1, 0.5]})
+        .add(content);
+
+    context
         .add({align: [0, 0.5]})
         .add(sideBar);
 
-    context
+    content
         .add(gameContainer);
+
+    context.on('resize', function(size){
+        contentSize.set([size[0]-sidebarWidth,size[1]], {duration: 500,curve: 'easeOut'});
+    });
 
     // Mount the context to a DOM element
     context.mount(document.body);
